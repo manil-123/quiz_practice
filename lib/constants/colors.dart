764feec0f09cc79674d8fec09f6074a800aeeb5d
Manil Pun <punmanil3@gmail.com>
2023-09-05 12:12:00 +0545

    question with timer

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const purpleColor = Color(0xFF5D458D);
+  static const lightPurpleColor = Color(0xFFE5E5E5);
   static const lightCircleColor = Color(0xFF8C79B2);
+  static const answerOptionColor = Color(0xFFF3F3F3);
 }
