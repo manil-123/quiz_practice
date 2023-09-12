896ba0908a35f918eb3c5a222378eed911c28b12
Manil Pun <punmanil3@gmail.com>
2023-09-12 15:14:57 +0545

    handle quiz cubit

---
+++ lib/constants/question_list.dart
@@ -0,0 +1,260 @@
+import 'package:quiz_practice/models/question.dart';
+
+final level1QuestionList = [
+  Question(
+    question: 'Level 1 Question 1',
+    options: [
+      'Question 1 Option 1',
+      'Question 1 Option 2',
+      'Question 1 Option 3',
+      'Question 1 Option 4',
+    ],
+    correctAnswer: 'Question 1 Option 1',
+  ),
+  Question(
+    question: 'Level 1 Question 2',
+    options: [
+      'Question 2 Option 1',
+      'Question 2 Option 2',
+      'Question 2 Option 3',
+      'Question 2 Option 4',
+    ],
+    correctAnswer: 'Question 2 Option 1',
+  ),
+  Question(
+    question: 'Level 1 Question 3',
+    options: [
+      'Question 3 Option 1',
+      'Question 3 Option 2',
+      'Question 3 Option 3',
+      'Question 3 Option 4',
+    ],
+    correctAnswer: 'Question 3 Option 1',
+  ),
+  Question(
+    question: 'Level 1 Question 4',
+    options: [
+      'Question 4 Option 1',
+      'Question 4 Option 2',
+      'Question 4 Option 3',
+      'Question 4 Option 4',
+    ],
+    correctAnswer: 'Question 4 Option 1',
+  ),
+  Question(
+    question: 'Level 1 Question 5',
+    options: [
+      'Question 5 Option 1',
+      'Question 5 Option 2',
+      'Question 5 Option 3',
+      'Question 5 Option 4',
+    ],
+    correctAnswer: 'Question 5 Option 1',
+  ),
+  Question(
+    question: 'Level 1 Question 6',
+    options: [
+      'Question 6 Option 1',
+      'Question 6 Option 2',
+      'Question 6 Option 3',
+      'Question 6 Option 4',
+    ],
+    correctAnswer: 'Question 6 Option 1',
+  ),
+  Question(
+    question: 'Level 1 Question 7',
+    options: [
+      'Question 7 Option 1',
+      'Question 7 Option 2',
+      'Question 7 Option 3',
+      'Question 7 Option 4',
+    ],
+    correctAnswer: 'Question 7 Option 1',
+  ),
+  Question(
+    question: 'Level 1 Question 8',
+    options: [
+      'Question 8 Option 1',
+      'Question 8 Option 2',
+      'Question 8 Option 3',
+      'Question 8 Option 4',
+    ],
+    correctAnswer: 'Question 8 Option 1',
+  ),
+  Question(
+    question: 'Level 1 Question 9',
+    options: [
+      'Question 9 Option 1',
+      'Question 9 Option 2',
+      'Question 9 Option 3',
+      'Question 9 Option 4',
+    ],
+    correctAnswer: 'Question 9 Option 1',
+  ),
+  Question(
+    question: 'Level 1 Question 10',
+    options: [
+      'Question 10 Option 1',
+      'Question 10 Option 2',
+      'Question 10 Option 3',
+      'Question 10 Option 4',
+    ],
+    correctAnswer: 'Question 10 Option 1',
+  ),
+];
+
+final level2QuestionList = [
+  Question(
+    question: 'Level 2 Question 1',
+    options: [
+      'Question 1 Option 1',
+      'Question 1 Option 2',
+      'Question 1 Option 3',
+      'Question 1 Option 4',
+    ],
+    correctAnswer: 'Question 1 Option 2',
+  ),
+  Question(
+    question: 'Level 2 Question 2',
+    options: [
+      'Question 2 Option 1',
+      'Question 2 Option 2',
+      'Question 2 Option 3',
+      'Question 2 Option 4',
+    ],
+    correctAnswer: 'Question 2 Option 2',
+  ),
+  Question(
+    question: 'Level 2 Question 3',
+    options: [
+      'Question 3 Option 1',
+      'Question 3 Option 2',
+      'Question 3 Option 3',
+      'Question 3 Option 4',
+    ],
+    correctAnswer: 'Question 3 Option 2',
+  ),
+  Question(
+    question: 'Level 2 Question 4',
+    options: [
+      'Question 4 Option 1',
+      'Question 4 Option 2',
+      'Question 4 Option 3',
+      'Question 4 Option 4',
+    ],
+    correctAnswer: 'Question 4 Option 2',
+  ),
+  Question(
+    question: 'Level 2 Question 5',
+    options: [
+      'Question 5 Option 1',
+      'Question 5 Option 2',
+      'Question 5 Option 3',
+      'Question 5 Option 4',
+    ],
+    correctAnswer: 'Question 5 Option 2',
+  ),
+  Question(
+    question: 'Level 2 Question 6',
+    options: [
+      'Question 6 Option 1',
+      'Question 6 Option 2',
+      'Question 6 Option 3',
+      'Question 6 Option 4',
+    ],
+    correctAnswer: 'Question 6 Option 2',
+  ),
+  Question(
+    question: 'Level 2 Question 7',
+    options: [
+      'Question 7 Option 1',
+      'Question 7 Option 2',
+      'Question 7 Option 3',
+      'Question 7 Option 4',
+    ],
+    correctAnswer: 'Question 7 Option 2',
+  ),
+  Question(
+    question: 'Level 2 Question 8',
+    options: [
+      'Question 8 Option 1',
+      'Question 8 Option 2',
+      'Question 8 Option 3',
+      'Question 8 Option 4',
+    ],
+    correctAnswer: 'Question 8 Option 2',
+  ),
+  Question(
+    question: 'Level 2 Question 9',
+    options: [
+      'Question 9 Option 1',
+      'Question 9 Option 2',
+      'Question 9 Option 3',
+      'Question 9 Option 4',
+    ],
+    correctAnswer: 'Question 9 Option 2',
+  ),
+  Question(
+    question: 'Level 2 Question 10',
+    options: [
+      'Question 10 Option 1',
+      'Question 10 Option 2',
+      'Question 10 Option 3',
+      'Question 10 Option 4',
+    ],
+    correctAnswer: 'Question 10 Option 2',
+  ),
+];
+
+final level3QuestionList = [
+  Question(
+    question: 'Level 3 Question 1',
+    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+    correctAnswer: 'Option 1',
+  ),
+  Question(
+    question: 'Level 3 Question 2',
+    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+    correctAnswer: 'Option 1',
+  ),
+  Question(
+    question: 'Level 3 Question 3',
+    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+    correctAnswer: 'Option 4',
+  ),
+  Question(
+    question: 'Level 3 Question 4',
+    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+    correctAnswer: 'Option 1',
+  ),
+  Question(
+    question: 'Level 3 Question 5',
+    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+    correctAnswer: 'Option 2',
+  ),
+  Question(
+    question: 'Level 3 Question 6',
+    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+    correctAnswer: 'Option 4',
+  ),
+  Question(
+    question: 'Level 3 Question 7',
+    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+    correctAnswer: 'Option 2',
+  ),
+  Question(
+    question: 'Level 3 Question 8',
+    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+    correctAnswer: 'Option 3',
+  ),
+  Question(
+    question: 'Level 3 Question 9',
+    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+    correctAnswer: 'Option 3',
+  ),
+  Question(
+    question: 'Level 3 Question 10',
+    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+    correctAnswer: 'Option 2',
+  ),
+];
