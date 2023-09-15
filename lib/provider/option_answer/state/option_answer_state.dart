a6883942bd24019fb494f09eaa988906655b30b7
Manil Pun <punmanil3@gmail.com>
2023-09-15 13:44:04 +0545

    wrong answer and timeout logic refactor.

--- lib/provider/option_answer/state/option_answer_state.dart
+++ lib/provider/option_answer/state/option_answer_state.dart
@@ -6,9 +6,12 @@ part 'option_answer_state.freezed.dart';
 class OptionAnswerState with _$OptionAnswerState {
   const factory OptionAnswerState.initial() = _OptionAnswerInitial;
 
+  // state for representing correct selected answer
   const factory OptionAnswerState.correct() = _OptionAnswerCorrect;
 
+  // state for representing wrong selected answer
   const factory OptionAnswerState.wrong() = _OptionAnswerWrong;
 
+  // state for representing no selected answer
   const factory OptionAnswerState.empty() = _OptionAnswerEmpty;
 }
--- lib/provider/quiz_page_provider.dart
+++ lib/provider/quiz_page_provider.dart
@@ -1,18 +1,26 @@
+import 'dart:developer';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:quiz_practice/app/state/generic_state.dart';
 import 'package:quiz_practice/constants/question_list.dart';
 import 'package:quiz_practice/models/quiz_page_state.dart';
+import 'package:quiz_practice/provider/option_answer/option_answer_provider.dart';
 
 final quizPageProvider =
     StateNotifierProvider<QuizPageNotifier, GenericState<QuizPageState>>((ref) {
-  return QuizPageNotifier();
+  return QuizPageNotifier(
+    ref.watch(
+      optionAnswerProvider.notifier,
+    ),
+  );
 });
 
 class QuizPageNotifier extends StateNotifier<GenericState<QuizPageState>> {
-  QuizPageNotifier()
+  QuizPageNotifier(this.optionAnswerProvider)
       : super(
           const GenericState<QuizPageState>.initial(),
         );
+
+  final OptionAnswerNotifier optionAnswerProvider;
 
   void loadQuizFirstTime() async {
     state = const GenericState<QuizPageState>.loading();
@@ -43,38 +51,51 @@ class QuizPageNotifier extends StateNotifier<GenericState<QuizPageState>> {
     state.maybeWhen(
       success: (quizPageState) {
         final selectedAnswer = quizPageState.selectedAnswer;
-        // When user does not select any answer
-        if (selectedAnswer == null) {
-          final remainingLives = quizPageState.lives;
-          // Check if the remaining lives is not zero.
-          if (remainingLives != 0) {
+        final remainingLives = quizPageState.lives;
+
+        // When user does not select any answer or the incorrect answer is selected.
+        if (selectedAnswer == null ||
+            selectedAnswer != quizPageState.currentQuestion.correctAnswer) {
+          // if the question number is 9 or 10 and no answer is selected or
+          // wrong answer is selected then game is over no matter the chances left.
+          if (quizPageState.currentQuestion.index > 7 ||
+              (quizPageState.currentQuestion.index < 8 &&
+                  remainingLives == 0)) {
+            Future.delayed(const Duration(milliseconds: 3000), () {
+              state = const GenericState<QuizPageState>.error("Game Over");
+            });
+          } else {
+            // It will need to reduce coin when remaining lives is available.
+            // reduceCoins();
+            //
+            //
+
+            // Decrement the remaining lives and update the state.
             final updatedState = quizPageState.copyWith(
               lives: remainingLives - 1,
             );
+            log("remaining lives ${updatedState.lives}");
+
             // Update the remaining lives by decreasing it as no answer is selected.
             state = GenericState<QuizPageState>.success(updatedState);
+
+            loadNextQuestion();
           }
-          checkForNextQuestion(quizPageState);
         } else {
-          if (selectedAnswer == quizPageState.currentQuestion.correctAnswer) {
-          } else {}
+          // If answer is correct then check for the next question.
+          loadNextQuestion();
         }
+
+        // This will show Correct/Wrong answer text in a container
+        // Also it will show correct answer with green background on the options list.
+        // For UI purpose only.
+        optionAnswerProvider.toggleAnswer(
+          selectedAnswer,
+          quizPageState.currentQuestion.correctAnswer,
+        );
       },
       orElse: () {},
     );
-  }
-
-  void checkForNextQuestion(QuizPageState quizPageState) {
-    // If it is not the last 2 questions and remaining lives is not zero
-    // Proceed to next question
-    if (quizPageState.currentQuestion.index < 8 && quizPageState.lives > 0) {
-      Future.delayed(const Duration(milliseconds: 3000), () {
-        loadNextQuestion();
-      });
-    } else {
-      // Any one of the last 2 question is not answered or lives is 0
-      state = const GenericState<QuizPageState>.error("Game Over");
-    }
   }
 
   void loadNextQuestion() {
@@ -89,8 +110,11 @@ class QuizPageNotifier extends StateNotifier<GenericState<QuizPageState>> {
           final updatedState = quizPageState.copyWith(
             index: nextQuestionIndex,
             currentQuestion: nextQuestion,
+            selectedAnswer: null,
           );
-          state = GenericState<QuizPageState>.success(updatedState);
+          Future.delayed(const Duration(milliseconds: 3000), () {
+            state = GenericState<QuizPageState>.success(updatedState);
+          });
         }
       },
       orElse: () {},
